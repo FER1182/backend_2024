98fe7ebdc96046f0e807ca790720cc034fc918f4
ferni232 <fernandorudnevich.inedita@gmail.com>
2024-03-06 18:28:30 -0300

    termine el getProduct de desafio clase4

--- entregas/desafio_clase4/index.js
+++ entregas/desafio_clase4/index.js
@@ -1,73 +1,93 @@
-const { createDiffieHellmanGroup } = require("crypto");
-const fs = require("fs")
-
+const fs = require("fs");
 
 class ProductManager {
-    constructor(path) {
-        this.products = [];
-        this.productIdCounter = 1; // Contador para el id autoincremental
-        this.path = path
+  constructor(path) {
+    this.products = [];
+    this.productIdCounter = 1; // Contador para el id autoincremental
+    this.path = path;
+  }
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+    // Validar que ninguna propiedad esté vacía
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.error("No se pueden agregar productos con propiedades vacías.");
+      return null;
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-        // Validar que ninguna propiedad esté vacía
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            console.error("No se pueden agregar productos con propiedades vacías.");
-            return null;
-        }
-
-        // Validar que no exista un producto con el mismo code
-        if (this.products.some(producto => producto.code === code)) {
-            console.error(`Ya existe un producto con el code ${code}.`);
-            return null;
-        }
-
-        const producto = {
-            id: this.productIdCounter++,
-            title: title,//nombre del producto
-            description: description, //descripcion del producto
-            price: price,
-            thumbnail: thumbnail,//ruta de imagen
-            code: code,//codigo identificador
-            stock: stock//numero de piezas disponibles
-        };
-        this.products.push(producto);
-
-        const guardarArchivos = async () => {
-            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
-        }
-        guardarArchivos();
-        return producto; // Retornamos el producto agregado
+    // Validar que no exista un producto con el mismo code
+    if (this.products.some((producto) => producto.code === code)) {
+      console.error(`Ya existe un producto con el code ${code}.`);
+      return null;
     }
-    getProductById(id) {
-        const producto = this.products.find(producto => producto.id === id);
-        if (producto) {
-            return producto;
-        } else {
-            console.error(`Error: Producto con id ${id} no encontrado.`);
-            return null;
-        }
+
+    const producto = {
+      id: this.productIdCounter++,
+      title: title, //nombre del producto
+      description: description, //descripcion del producto
+      price: price,
+      thumbnail: thumbnail, //ruta de imagen
+      code: code, //codigo identificador
+      stock: stock, //numero de piezas disponibles
+    };
+    this.products.push(producto);
+
+    const guardarArchivos = async () => {
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(this.products, null, 2)
+      );
+    };
+    guardarArchivos();
+    return producto; // Retornamos el producto agregado
+  }
+  getProductById(id) {
+    const producto = this.products.find((producto) => producto.id === id);
+    if (producto) {
+      return producto;
+    } else {
+      console.error(`Error: Producto con id ${id} no encontrado.`);
+      return null;
     }
-    getProducts() {
-        const leerArchivos = async () => {
-            const respuesta = await fs.promises.readFile(this.path, "utf-8");
-            const nuevoArray = JSON.parse(respuesta);
-            console.log(nuevoArray);
-        }
-        leerArchivos();
-        
+  }
+  getProducts() {
+    if (fs.existsSync(this.path)) {
        
+      const leerArchivos = async () => {
+        const respuesta = await fs.promises.readFile(this.path, "utf-8");
+        const nuevoArray = JSON.parse(respuesta);
+        console.log(nuevoArray);
+      };
+
+      leerArchivos();
+    } else {
+        
+      console.log("no existe el archivo");
     }
+  }
 }
 
 // Ejemplo de uso:
 const manager = new ProductManager("./productos.json");
-//Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+//Se llamará “getProducts” recién creada la instancia, debe devolver no existe el archivo
 manager.getProducts();
 
 // Agrego producto de prueba
-//manager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc123", 25);
-//manager.addProduct("producto prueba2", "Este es un producto prueba2", 200, "sin imagen", "abc1232", 125);
+manager.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "sin imagen",
+  "abc123",
+  25
+);
+manager.addProduct(
+  "producto prueba2",
+  "Este es un producto prueba2",
+  200,
+  "sin imagen",
+  "abc1232",
+  125
+);
 // Muestra el producto agregado
 manager.getProducts();
 
