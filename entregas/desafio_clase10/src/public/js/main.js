36622c20e102aece2a7ca25a41b9235b23117d6c
FER1182 <fernandorudnevich.inedita@gmail.com>
2024-04-01 14:15:49 -0300

    desafio clase 10 avance

--- entregas/desafio_clase10/src/public/js/main.js
+++ entregas/desafio_clase10/src/public/js/main.js
@@ -1,17 +1,26 @@
 const socket = io()
 
 
-socket.on("productos", (data) =>{
-    const contenedorProductos= document.getElementById("contenedorProductos");
-    contenedorProductos.innerHTML= "";
+socket.on("productos", (data) => {
+    const contenedorProductos = document.getElementById("contenedorProductos");
+    contenedorProductos.innerHTML = "";
     data.forEach(item => {
         const card = document.createElement("div");
         card.innerHTML = `
-                                
-
-        
-        `
+                            <p> Id: ${item.id}</p>
+                            <p> Titulo: ${item.title}</p>
+                            <p> Precio: ${item.price}</p>
+                            <button> Eliminar Producto </button>
+            `
+        contenedorProductos.appendChild(card)
+        card.querySelector("button").addEventListener("click", () => {
+            eliminarProducto(item.id)
+        })
 
     })
-    
+
 })
+
+const eliminarProducto = (id) =>{
+    socket.emit("eliminarProducto",id)
+}
