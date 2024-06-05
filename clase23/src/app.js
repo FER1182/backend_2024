03f437c9c0dbce9266605dd38f03f41ff7beb587
ferni232 <fernandorudnevich.inedita@gmail.com>
2024-06-05 20:49:59 -0300

    clase23 min 15

---
+++ clase23/src/app.js
@@ -0,0 +1,5 @@
+//expresiones regulares validar si el texto ingresado por el usuario corresponde a un mail
+// email:nombre@dominio.com
+
+let correoIngresado = "lionel@mesi.com"
+let correoFalso = "tinkiwinik"
