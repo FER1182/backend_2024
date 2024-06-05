aba4413c29bc573566c0ecddbf19b1595193bcda
ferni232 <fernandorudnevich.inedita@gmail.com>
2024-06-04 23:03:08 -0300

    clase22

--- clase22/src/app.js
+++ clase22/src/app.js
@@ -1,5 +1,8 @@
 import express from "express"
 import jwt from "jsonwebtoken"
+import cookieParser from "cookie-parser"
+import initializePassport from "./config/passport.config.js"
+import passport from "passport"
 const app= express()
 const PUERTO = 8080
 
@@ -9,6 +12,10 @@ const PUERTO = 8080
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static("./src/public"));
+app.use(cookieParser())
+app.use(passport.initialize());
+initializePassport();
+
 
 //rutas
 app.post("/login",(req,res)=>{
@@ -17,10 +24,20 @@ app.post("/login",(req,res)=>{
     if(usuario==="tinki" && pass === "winki"){
         //creo el token
     let token = jwt.sign({usuario,pass},"coderhouse",{expiresIn: "24h"})
-    res.send({message :"login exitoso",token});
+    //res.send({message :"login exitoso",token});
+       //****envio tooken desde cookie */ 
+        res.cookie("coderCookieToken",token,{
+            maxAge: 60*60*60*1000,
+            httpOnly: true,//solo se accede desde una http
+        }).send({message :"login exitos"})
+
     }else{
         res.send({message :"login fallido"});
     }
+})
+
+app.get("/current",passport.authenticate("jwt",{session:false}),(req,res)=>{
+    res.send(req.user)
 })
 
 
