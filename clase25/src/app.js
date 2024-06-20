5db23378ed8c64ef7a6f7b2b055115098fa0f1c8
fer1182 <fernandorudnevich.inedita@gmail.com>
2024-06-20 14:04:57 -0300

    clase 25 min 42

--- clase25/src/app.js
+++ clase25/src/app.js
@@ -1,22 +1,32 @@
-//objeto process cada vez que ejecuto la consola se crea el objeto process
-//console.log(process)
+// //objeto process cada vez que ejecuto la consola se crea el objeto process
+// //console.log(process)
 
-//si quiero saber el directorio donde se ejecuta la raiz de la aplicacions
-console.log(process.cwd())
+// //si quiero saber el directorio donde se ejecuta la raiz de la aplicacions
+// console.log(process.cwd())
 
-//obtener el id del process
-console.log(process.pid)
+// //obtener el id del process
+// console.log(process.pid)
 
-//memoria que usa el proceso
-console.log(process.memoryUsage())
+// //memoria que usa el proceso
+// console.log(process.memoryUsage())
 
-//que version de node estamos usando
-console.log(process.version)
+// //que version de node estamos usando
+// console.log(process.version)
 
-//para finalizar un proceso
+// //para finalizar un proceso
 
-//process.exit()
-//console.log("texto adicional")//lo que esta despues del exit nunca se muestra
+// //process.exit()
+// //console.log("texto adicional")//lo que esta despues del exit nunca se muestra
 
-// node src/app.js tinkiwinki en el argv va a aparecer tambien tinkiwinki
-console.log(process.argv)
+// // node src/app.js tinkiwinki en el argv va a aparecer tambien tinkiwinki
+// console.log(process.argv)
+import express from "express"
+import UserModel from "./models/usuarios.model.js";
+const app = express();
+import mongoose from "mongoose";
+
+app.get("/",(req,res)=>{
+    res.send("olisss")
+})
+
+app.listen(8080)
---
+++ clase25/src/utils/commander.js
@@ -0,0 +1,17 @@
+//nos permite configurar nuestros argumentos de consola
+//npm i commander
+import { Command } from "commander";
+
+
+const program = new Command()
+
+
+// tres argunmetno 1-comando 2-descripcion 3 - valor por default
+
+program
+    .option("-p <port>","puerto en donde se inicia el servidor",8080)
+    .option("--mode <mode>","modo de trabajo","produccion")
+program.parse();    
+
+console.log("opciones",program.opts());
+//node src/utils/commander.js con este comando me aparece lo q le pase
