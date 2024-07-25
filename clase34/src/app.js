b61042f36184885dea8ab875875397082eb32fe7
ferni232 <fernandorudnevich.inedita@gmail.com>
2024-07-24 22:03:41 -0300

    clase 34

--- clase34/src/app.js
+++ clase34/src/app.js
@@ -33,7 +33,39 @@ app.get("/", (req, res) => {
     res.send("Hola mundo");
 })
 
+//simulamos algunas peticiones para artillery
+//OPERACION SIMPLE
+app.get("/operacionSimple", (req, res) => {
+    let suma = 0;
+
+    for (let i = 0; i < 1000000; i++) {
+        suma += i;
+    }
+
+    res.send(`la suma es ${suma}`);
+})
+
+//OEPRACION COMPLEJA
+
+app.get("/operacionCompleja", (req, res) => {
+
+    let suma = 0;
+    for (let i = 0; i < 5e8; i++) {
+        suma += i;
+    }
+
+    res.send(`la suma es ${suma}`);
+        
+})
+
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 })  
+
+
+//artillery 
+//npm i artillery
+//artillery quick --count 40 --num 50 "http://localhost:8080/operacionsimple" -o simple.json
+
+//artillery quick --count 40 --num 50 "http://localhost:8080/operacioncompleja" -o compleja.json
